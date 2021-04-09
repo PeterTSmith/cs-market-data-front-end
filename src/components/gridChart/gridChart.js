52ddaaed090cbac2d9d652d9c03374b000bd381d
Peter Smith <peter.t.smith.email@gmail.com>
2021-04-09 00:10:22 -0400

    GridChart WIP

--- src/components/gridChart/gridChart.js
+++ src/components/gridChart/gridChart.js
@@ -3,15 +3,39 @@ import React from 'react';
 import './gridChart.css'
 
 export default class GridChart extends React.Component {
+    constructor() {
+        super();
+        this.state = {gridRow: [['a0', 'a1', 'a2'], ['b0', 'b1', 'b2']]};
+    }
+
+    componentDidMount() {
+    }
+
     render() {
         return(
             <div id="gridChart">
-                <div id="gridRow">
-                    <div id="gridColumn">
-                        ff
-                    </div>
-                </div>
+                {this.generateRow()}
             </div>
         );
+    }
+
+    generateRow() {
+        return this.state.gridRow.map((row) => {
+            return (
+                <div id="gridRow">
+                    {this.generateColumn(row)}
+                </div>
+            );
+        });
+    }
+
+    generateColumn(rowIn) {
+        return rowIn.map((column) => {
+            return (
+                <div id="gridColumn">
+                    {column}
+                </div>
+            );
+        });
     }
 }
